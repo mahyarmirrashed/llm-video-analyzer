521de5855636e5991d084f5cd1616482da674a2a
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-04-22 14:59:57 -0700

    feat: add skeleton for parse command

---
+++ cli/parse.go
@@ -0,0 +1,27 @@
+package cli
+
+import "github.com/urfave/cli/v2"
+
+func ParseCommand(cfg *Config) *cli.Command {
+	return &cli.Command{
+		Name:  "parse",
+		Usage: "Process a video into database",
+		Flags: []cli.Flag{
+			&cli.IntFlag{
+				Name:        "sampling-interval",
+				Value:       2,
+				Usage:       "Frame sampling interval (seconds)",
+				Destination: &cfg.SamplingInterval,
+			},
+			&cli.StringFlag{
+				Name:        "sampling-model",
+				Value:       "llava",
+				Usage:       "Frame sampling model for analysis",
+				Destination: &cfg.SamplingModel,
+			},
+		},
+		Action: func(c *cli.Context) error {
+			return nil // TODO
+		},
+	}
+}
