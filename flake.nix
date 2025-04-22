a2a15e121297277b754facfc199fc1e0446581cb
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-04-22 12:17:51 -0700

    chore(nix): add basic flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Flake for github:mahyarmirrashed/llm-video-analyzer";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        mahyarmirrashed-llm-video-analyzer = pkgs.stdenv.mkDerivation {
+          pname = "mahyarmirrashed-llm-video-analyzer";
+          version = "0.1.0";
+          src = self;
+        };
+      in {
+        formatter = pkgs.alejandra;
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [git lazygit];
+        };
+
+        packages.default = mahyarmirrashed-llm-video-analyzer;
+      }
+    );
+}
