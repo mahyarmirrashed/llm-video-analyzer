61b43ac5efd76f9d2f4206b872042a8e18158347
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-04-23 02:21:38 -0700

    feat: add flag for selecting embedding model

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,6 +3,7 @@ package config
 type Config struct {
 	SamplingInterval int
 	SamplingModel    string
+	EmbeddingModel   string
 	OllamaURL        string
 	DatabaseURL      string
 	Debug            bool
