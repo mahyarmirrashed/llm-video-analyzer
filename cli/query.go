e0e121032fa041d2fd832ec4942f041d0e5ef095
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-04-24 18:04:01 -0700

    feat: implement logic for query method

--- cli/query.go
+++ cli/query.go
@@ -1,7 +1,11 @@
 package cli
 
 import (
+	"fmt"
+
 	"github.com/mahyarmirrashed/llm-video-analyzer/pkg/config"
+	"github.com/mahyarmirrashed/llm-video-analyzer/pkg/ollama"
+	"github.com/mahyarmirrashed/llm-video-analyzer/pkg/qdrant"
 	"github.com/urfave/cli/v2"
 )
 
@@ -24,7 +28,38 @@ func QueryCommand(cfg *config.Config) *cli.Command {
 			},
 		},
 		Action: func(c *cli.Context) error {
-			return nil // TODO
+			query := c.Args().First()
+			if query == "" {
+				return fmt.Errorf("search query required")
+			}
+
+			desc, err := ollama.GetDescriptionFromQuery(c.Context, cfg, query)
+			if err != nil {
+				return fmt.Errorf("failed to get description: %w", err)
+			}
+
+			embedding, err := ollama.GetTextEmbedding(c.Context, cfg, desc)
+			if err != nil {
+				return fmt.Errorf("failed to get embedding: %w", err)
+			}
+
+			dbClient, err := qdrant.New(cfg.DatabaseURL)
+			if err != nil {
+				return fmt.Errorf("failed to connect to database: %w", err)
+			}
+
+			pts, err := dbClient.Search(c.Context, embedding, uint64(cfg.QueryLimit))
+			if err != nil {
+				return fmt.Errorf("search failed: %w", err)
+			} else if len(pts) == 0 {
+				return fmt.Errorf("no results found")
+			}
+
+			for _, res := range pts {
+				fmt.Printf("Result: %v", res)
+			}
+
+			return nil
 		},
 	}
 }
